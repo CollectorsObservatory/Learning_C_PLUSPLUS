3edd8f58fbb61572c404fc9dc040241db18a8776
Fares Majdoub <126903635+CollectorsObservatory@users.noreply.github.com>
2023-10-15 02:20:16 -0400

    Update validationFormat.h

--- SchoolProjects/validationFormat.h
+++ SchoolProjects/validationFormat.h
@@ -1,3 +1,15 @@
+/**
+ * @file validationFormat.h
+ * @author CollectorsObservatory
+ * @date 2023-10-15
+ *
+ * @brief Ce fichier d'en-tête déclare des fonctions pour la validation des formats de numéros de téléphone et RAMQ.
+ *
+ * Il déclare des fonctions qui aident à vérifier la validité des formats de numéros en se basant sur des règles
+ * spécifiques. Les règles comprennent la longueur correcte, les caractères autorisés et le format général des numéros.
+ * Les constantes nécessaires pour la validation, telles que la longueur et le nombre de codes régionaux, sont également définies.
+ *
+ */
 #ifndef VALIDATION_H_
 #define VALIDATION_H_
 
